efaa3a2773f253c57d6422be5affb2cf8a0d2d72
dwzm0 <dwzm00gmail.com>
2023-03-27 22:06:36 +0300

    part5 changes

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,9 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const jwt = require("jsonwebtoken");
+
 const middleware = require("../utils/middleware");
+const { default: mongoose } = require("mongoose");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
@@ -21,7 +24,7 @@ blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user.id,
+    user: user,
   });
 
   const savedBlog = await blog.save();
@@ -43,26 +46,30 @@ blogsRouter.delete(
 
     if (blog.user._id.toString() === user._id.toString()) {
       await Blog.findByIdAndRemove(request.params.id);
-      response.status(204).end();
+      response.status(200).end();
     } else {
       return response.status(401).end();
     }
   }
 );
 
-blogsRouter.put("/:id", async (request, response) => {
+blogsRouter.put("/:id", middleware.userExtractor, async (request, response) => {
   const body = request.body;
+  console.log(request.params);
 
   const blog = {
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: body.user,
   };
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
     new: true,
   });
+
+  console.log(updatedBlog);
   response.status(200).json(updatedBlog);
 });
 
