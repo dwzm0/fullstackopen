cd286684cf35ae00269f29bd60be21bcaaf1a9bf
dwzm0 <dwzm00gmail.com>
2023-04-25 13:22:55 +0300

    separated diagnoses component

--- part9/patientor/src/components/AddEntryPage/addOccupationalHealthcareEntryForm.tsx
+++ part9/patientor/src/components/AddEntryPage/addOccupationalHealthcareEntryForm.tsx
@@ -1,14 +1,9 @@
-import { useState, SyntheticEvent, useEffect } from "react";
+import { useState, SyntheticEvent,  } from "react";
 import {  TextField, Grid, Button, } from '@mui/material';
-import { OccupationalHealthcareEntryWithoutId, Diagnosis, Patient, NonSensitiveDiagnoseEntry } from "../../types"
+import { OccupationalHealthcareEntryWithoutId, Patient, NonSensitiveDiagnoseEntry, Diagnosis} from "../../types"
 import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemText from '@mui/material/ListItemText';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
+
 import Input from '@mui/material/Input';
-import diagnoseService from "../../services/diagnoses"
 import DiagosesComponent from "../../hooks";
 
 
@@ -17,43 +12,17 @@ interface Props {
     patient: Patient,
   }
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 300,
-    },
-  },
-}; 
+
 
 const AddOccupationalHealthcareEntryForm = ({onSubmit, patient }: Props) => {
     const [diagnoses, setDiagnoses] = useState<NonSensitiveDiagnoseEntry[]>([]);
+    const [diagnosisCodes, setDiagnosisCodes] = useState<Diagnosis['code'][]>([])
     const [description, setDescription] = useState("")
     const [date, setDate] = useState("")
     const [specialist, setSpecialist] = useState("")
-    const [diagnosisCodes, setDiagnosisCodes] = useState<Diagnosis['code'][]>([])
     const [employerName, setEmployerName] = useState("")
     const [startDate, setStartDate] = useState("")
     const [endDate, setEndDate] = useState("")
-
-    const handleDiagnosisChange = (event: SelectChangeEvent<typeof diagnosisCodes>) => {
-      const {
-        target: { value },
-      } = event;
-      setDiagnosisCodes(
-        typeof value === 'string' ? value.split(',') : value,
-      );
-    };
-
-    useEffect(() => {
-      const fetchDiagnoseList = async () => {
-        const diagnoses = await diagnoseService.getAll();
-        setDiagnoses(diagnoses);
-      };
-      void fetchDiagnoseList();
-    }, []);
 
     const addEntry = (event: SyntheticEvent) => {
         event.preventDefault();
@@ -111,25 +80,7 @@ const AddOccupationalHealthcareEntryForm = ({onSubmit, patient }: Props) => {
               value={specialist}
               onChange={({ target }) => setSpecialist(target.value)}
             />
-           <InputLabel id="diagnose-multiple-checkbox-label">Select Diagnose Code</InputLabel> 
-           <Select
-              sx={{width: 300}}
-              id="diagnose-multiple-checkbox"
-              multiple
-              value={diagnosisCodes}
-              onChange={handleDiagnosisChange}
-              input={<OutlinedInput label="Select Diagnose Code" />}
-              renderValue={(selected) => selected.join(', ')}
-              MenuProps={MenuProps}
-            >
-          {diagnoses.map((diagnose) => (
-            <MenuItem key={diagnose.code} value={diagnose.code}>
-              <Checkbox checked={diagnosisCodes.indexOf(diagnose.code) > -1} />
-              <ListItemText primary={diagnose.code} />
-            </MenuItem>
-          ))}
-          <DiagosesComponent />
-        </Select>
+        <DiagosesComponent diagnoses={diagnoses} setDiagnoses={setDiagnoses} diagnosisCodes={diagnosisCodes} setDiagnosisCodes={setDiagnosisCodes}/>
               <TextField
               label="Employer name"
               fullWidth
--- part9/patientor/src/hooks/index.tsx
+++ part9/patientor/src/hooks/index.tsx
@@ -1,12 +1,19 @@
-import { useState, useEffect } from "react";
+import {useEffect, Dispatch, SetStateAction } from "react";
 import { NonSensitiveDiagnoseEntry, Diagnosis } from "../types"
 import diagnoseService from "../services/diagnoses"
 import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import ListItemText from '@mui/material/ListItemText';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 
+interface Props {
+    diagnoses: NonSensitiveDiagnoseEntry[];
+    setDiagnoses: Dispatch<SetStateAction<NonSensitiveDiagnoseEntry[]>>,
+    diagnosisCodes: Diagnosis['code'][],
+    setDiagnosisCodes: Dispatch<SetStateAction<string[]>>,
+  }
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -19,10 +26,7 @@ const MenuProps = {
   },
 }; 
 
-  const DiagosesComponent = () => {
-    const [diagnoses, setDiagnoses] = useState<NonSensitiveDiagnoseEntry[]>([]);
-    const [diagnosisCodes, setDiagnosisCodes] = useState<Diagnosis['code'][]>([])
-
+  const DiagosesComponent = ({diagnoses, setDiagnoses, diagnosisCodes, setDiagnosisCodes}: Props) => {
 
     const handleDiagnosisChange = (event: SelectChangeEvent<typeof diagnosisCodes>) => {
         const {
@@ -40,11 +44,10 @@ const MenuProps = {
     };
     void fetchDiagnoseList();
     }, []);
-
-    console.log("HI");
     
-
     return (
+        <>
+        <InputLabel id="diagnose-multiple-checkbox-label">Select Diagnose Code</InputLabel> 
         <Select
         sx={{width: 300}}
         id="diagnose-multiple-checkbox"
@@ -62,6 +65,7 @@ const MenuProps = {
       </MenuItem>
     ))}
   </Select>
+  </>
     )
 }
 
